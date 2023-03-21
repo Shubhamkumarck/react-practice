e717a8d876afb4c950c9c2a2816f7f9a75412a6f
Shubham Kumar <shubham.kumar@cloudkaptan.com>
2023-03-21 09:50:20 +0530

    adding css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
-    return <h1>Hello There!!</h1>
+    return (<div className='parent-div'>
+            <div className='expense-date'>2022-01-27</div>
+                <div className='expense-name'>
+                    <h2>
+                        Car Insurance
+                    </h2>
+                </div>
+                <div className='expense-amount'>$500</div>
+        </div>);
 }
 
 export default ExpenseItem;
