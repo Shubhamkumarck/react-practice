0ef179aa94afd1862c92d92a7ccac9f3f6addfab
Shubham Kumar <shubham.kumar@cloudkaptan.com>
2023-03-21 09:20:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div className="App">
       <h2> Let's Get Started !!</h2>
+      <p>This is also visible!!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
